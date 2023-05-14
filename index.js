2cd60c691932631493fca89743130e89eb6e65d3
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-05-14 09:37:48 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,135 @@
+// grab all elements 
+const form = document.querySelector("[data-form]");
+const lists = document.querySelector("[data-lists]");
+const input = document.querySelector("[data-input]");
+const removeAllBtn=document.querySelector(".removeAll_btn")
+//local Storage
+class Storage {
+    static addTodStorage(todoArr){
+        let storage = localStorage.setItem("todo", JSON.stringify(todoArr));
+        return storage;
+    }
+
+    static getStorage(){
+        let storage = localStorage.getItem("todo") === null ? 
+        [] : JSON.parse(localStorage.getItem("todo"));
+        return storage
+    }
+}
+
+// empty array 
+let todoArr = Storage.getStorage();
+
+// form part 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let id = Math.random()
+    const todo = new Todo(id, input.value);
+    todoArr = [...todoArr, todo];
+    UI.displayData();
+    UI.clearInput();
+    //add to storage
+    Storage.addTodStorage(todoArr);
+});
+
+// make object instance 
+class Todo {
+    constructor(id, todo){
+        this.id = id;
+        this.todo = todo;
+    }
+}
+
+// display the todo in the DOM;
+class UI{
+    static displayData(){
+        let displayData = todoArr.map((item) => {
+            return `
+                <div class="todo">
+                <p>${item.todo}</p>
+                <span class="remove" data-id = ${item.id}>🗑️</span>
+                </div>
+            `
+        });
+        lists.innerHTML = (displayData).join(" ");
+    }
+    static clearInput(){
+        input.value = "";
+    }
+    static removeTodo(){
+        lists.addEventListener("click", (e) => {
+            if(e.target.classList.contains("remove")){
+                e.target.parentElement.remove();
+            }
+            let btnId = e.target.dataset.id;
+            //remove from array.
+            UI.removeArrayTodo(btnId);
+        });
+    }
+    static removeArrayTodo(id){
+        todoArr = todoArr.filter((item) => item.id !== +id);
+        Storage.addTodStorage(todoArr);
+    }
+
+
+static removeAll(){
+    removeAllBtn.addEventListener("click",()=>{
+todoArr.length=0;
+localStorage.clear();
+UI.displayData();
+    
+})
+}
+}
+
+
+    
+    
+
+
+
+
+
+
+
+//once the browser is loaded
+window.addEventListener("DOMContentLoaded", () => {
+    UI.displayData();
+    //remove from the dom
+    UI.removeTodo();
+    UI.removeAll();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
